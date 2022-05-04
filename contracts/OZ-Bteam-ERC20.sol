0268e819f8e57a6fe50076eea6ee3307e18b1e7c
CesRC <ces.rodriguez.c@gmail.com>
2022-05-04 15:37:46 +0200

    Added all dependencies

--- contracts/OZ-Bteam-ERC20.sol
+++ contracts/OZ-Bteam-ERC20.sol
@@ -3,9 +3,15 @@
 
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+// Dependencies for npm package
+/* import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
-import "@openzeppelin/contracts/utils/Context.sol";
+import "@openzeppelin/contracts/utils/Context.sol"; */
+
+// Dependencies for Remix
+import "github.com/OpenZeppelin/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+import "github.com/OpenZeppelin/openzeppelin-contracts/contracts/token/ERC20/extensions/IERC20Metadata.sol";
+import "github.com/OpenZeppelin/openzeppelin-contracts/contracts/utils/Context.sol";
 
 /**
  * @dev Implementation of the {IERC20} interface.
